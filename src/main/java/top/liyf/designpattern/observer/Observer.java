6ec048140c282e2465c31e5467e4abbae4858315
93814690 <93814690@qq.com>
2019-04-29 00:18:40 +0800

    添加观察者模式支持

---
+++ src/main/java/top/liyf/designpattern/observer/Observer.java
@@ -0,0 +1,18 @@
+package top.liyf.designpattern.observer;
+
+/**
+ * @author liyf
+ * Created in 2019-04-28
+ */
+public interface Observer {
+
+    /**
+     * 功能描述:
+     *
+     * @param subject 被观察者
+     * @param arg 参数
+     * @return void
+     * @author liyf
+     */
+    void update(Subject subject, Object arg);
+}
---
+++ src/main/java/top/liyf/designpattern/observer/Subject.java
@@ -0,0 +1,73 @@
+package top.liyf.designpattern.observer;
+
+/**
+ * @author liyf
+ * Created in 2019-04-28
+ */
+public interface Subject {
+
+    /**
+     * 功能描述: 注册观察者
+     *
+     * @param o 观察者
+     * @return void
+     * @author liyf
+     */
+    void registerObserver(Observer o);
+
+    /**
+     * 功能描述: 删除观察者
+     *
+     * @param o 观察者
+     * @return void
+     * @author liyf
+     */
+    void removeObserver(Observer o);
+
+    /**
+     * 功能描述: 通知所有的观察者
+     *
+     * @param
+     * @return void
+     * @author liyf
+     */
+    default void notifyObserver() {
+        notifyObserver(null);
+    }
+
+    /**
+     * 功能描述: 通知所有的观察者
+     *
+     * @param arg 推送的数据
+     * @return void
+     * @author liyf
+     */
+    void notifyObserver(Object arg);
+
+    /**
+     * 功能描述: 标记被观察者对象已经改变
+     *
+     * @param
+     * @return void
+     * @author liyf
+     */
+    void setChanged();
+
+    /**
+     * 功能描述:清除改变状态
+     *
+     * @param
+     * @return void
+     * @author liyf
+     */
+    void clearChanged();
+
+    /**
+     * 功能描述: 查询被观察者对象是否改变
+     *
+     * @param
+     * @return boolean
+     * @author liyf
+     */
+    boolean hasChanged();
+}
